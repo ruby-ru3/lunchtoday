7b0cf168f4ae51b12f7d1801778758b563d6b0f3
ruby-ru3 <dreamwoozu@gmail.com>
2024-06-17 16:39:55 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,7 +112,7 @@ def recommend_menu(menus):
     if menus:
         recommendations = random.sample(menus, min(3, len(menus)))
         prompt = f"추천 점심 메뉴: {', '.join(recommendations)}\n이 메뉴는 어떠세요? 마음에 드는 점심을 골라 보세요! 😊"
-        response = menu_generator(prompt)[0]['generated_text']
+        response = menu_generator(prompt, max_new_tokens=100)[0]['generated_text']
         return f"{prompt}\n\n{response.replace(prompt, '')}"
     else:
         return "추천할 메뉴가 없네요. 다른 카테고리를 선택해보세요!"
